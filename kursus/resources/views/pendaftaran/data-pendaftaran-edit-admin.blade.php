b8d9ecd10d5b05833da3d22d17c2611cac6df8c9
Fugomika <farrasfadhila76@gmail.com>
2024-02-04 00:45:23 +0700

    add : merek (buat tempalate)

--- kursus/resources/views/pendaftaran/data-pendaftaran-edit-admin.blade.php
+++ kursus/resources/views/pendaftaran/data-pendaftaran-edit-admin.blade.php
@@ -15,42 +15,8 @@
     <h1 class="font-weight-bold" style="font-size: 24px;">Edit Pendaftaran Garansi</h1>
 @endsection
 
-@section('menu')
-@auth
-<ul class="metismenu" id="menu">
-    <li><a href="{{route('dashboard')}}">
-            <i class="fas fa-home"></i>
-            <span class="nav-text">Garansi</span>
-        </a>
-    </li>
-    @if (auth()->user()->role == 'Administrator')
-    <li><a class="has-arrow" href="javascript:void()" aria-expanded="false">
-        <i class="fa fa-book"></i>
-        <span class="nav-text">Data Master</span>
-        </a>
-        <ul aria-expanded="false">
-            <li><a href="{{route('data-user')}}">Garansi</a></li>
-            <li><a href="{{route('data-sekolah')}}">Merek & Tipe Mobil</a></li>
-            <li><a href="{{route('data-produk')}}">Produk</a></li>
-        </ul>
-    </li>
-    <li><a class="has-arrow" href="javascript:void()" aria-expanded="false">
-        <i class="fa fa-database"></i>
-        <span class="nav-text">Data History</span>
-        </a>
-        <ul aria-expanded="false">
-            <li><a href="{{route('data-registration')}}">Pendaftaran Garansi</a></li>
-        </ul>
-    </li>
-    @else
-    <li class="mm-active"><a href="{{ route('data-registration') }}" aria-expanded="false">
-            <i class="fa fa-database"></i>
-            <span class="nav-text">Pendaftaran</span>
-        </a>
-    </li>
-    @endif
-</ul>
-@endauth
+@section('menu')    
+    @include('sidebar')
 @endsection
 
 @section('content')
